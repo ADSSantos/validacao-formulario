981fdab7f605603fccbee64ca7e9d0fa4484b83a
ADSSantos <121822984+ADSSantos@users.noreply.github.com>
2024-11-10 18:48:03 +0000

    componente AddCliente

---
+++ validacao-formulario/src/App.js
@@ -0,0 +1,15 @@
+/*import logo from './logo.svg';*/
+import './App.css';
+import AddCliente from './componentes/form';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AddCliente/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ validacao-formulario/src/componentes/form.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const AddCliente = () => {
+
+
+
+  return <>
+    <h1>ola teste</h1>
+  </>
+
+ 
+}
+
+export default AddCliente;
