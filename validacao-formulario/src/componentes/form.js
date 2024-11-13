0b617bec7d4bb6ff88601f930e195e89249cc27f
ADSSantos <121822984+ADSSantos@users.noreply.github.com>
2024-11-13 09:55:25 +0000

    formik em funcionamento

--- validacao-formulario/src/componentes/form.js
+++ validacao-formulario/src/componentes/form.js
@@ -4,23 +4,45 @@ import { Formik } from "formik";
 const AddCliente = () => {
   return (
     <>
-      <h1>Teste</h1>
-      <Formik>
-        <form noValidate>
-          <div className="form-group">
-            <label htmlFor="nome">Nome:</label>
-            <input type="text" id="nome" name="nome"></input>
-          </div>
-          <div className="form-group">
-            <label htmlFor="email">Email:</label>
-            <input type="text" id="email" name="email"></input>
-          </div>
-          <div className="form-group">
-            <label>Data Nascimento:</label>
-            <input type="date" id="data" name="data"></input>
-          </div>
-          <button type="submit">Adicionar</button>
-        </form>
+      <h1>Cadastro</h1>
+      <Formik
+        initialValues={{ nome: "", email: "", data: "" }}
+        onSubmit={(galinhaPintadinha) => {
+          console.log(galinhaPintadinha);
+        }}
+      >
+        {(props) => (
+          <form onSubmit={props.handleSubmit} noValidate>
+            <div className="form-group">
+              <label htmlFor="nome">Nome:</label>
+              <input
+                type="text"
+                id="nome"
+                name="nome"
+                onChange={props.handleChange}
+              ></input>
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email:</label>
+              <input
+                type="text"
+                id="email"
+                name="email"
+                onChange={props.handleChange}
+              ></input>
+            </div>
+            <div className="form-group">
+              <label>Data Nascimento:</label>
+              <input
+                type="date"
+                id="data"
+                name="data"
+                onChange={props.handleChange}
+              ></input>
+            </div>
+            <button type="submit">Adicionar</button>
+          </form>
+        )}
       </Formik>
     </>
   );
